78eea3c10c00853dc495ceb4014a1f82fc1165e3
andrew <andrewkalumba29@gmail.com>
2025-08-12 22:10:56 +0200

    corrected spelling mistake

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -8,7 +8,7 @@ const HomePage = () => {
         <div className="flex justify-center items-center bg-[#466995]">
             <div className="w-[30%]">
                <div className="flex justify-center text-base uppercase p-1.5 font-bold text-amber-50"> <ReusableH2 title="frontend developer" /> </div>
-                <ReusableInfo text="I specialize in building exceptional digital experience. With a focus on performance and accessibility, I transform designs into high quality products that delight users and make a postive impact." />
+                <ReusableInfo text="I specialize in building exceptional digital experience. With a focus on performance and accessibility, I transform designs into high quality products that delight users and make a positive impact." />
             </div>
             <img src="./image2.jpg" alt="image" width={50} height={50} className="w-[30%] rounded-full p-4"/>
         </div>
